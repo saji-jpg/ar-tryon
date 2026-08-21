3dcf0bc6b6860962f642d7e6f6288dcf7777f701
saji-jpg <sazi0704@gmail.com>
2026-08-21 13:45:05 +0900

    夏服の表示サイズを5.5に統一
    * 夏服の表示サイズを5.5に変更
    
    * ARスクリプトのキャッシュを更新

--- app-v3.js
+++ app-v3.js
@@ -20,7 +20,7 @@ const LEFT_EYE = [33, 133, 159, 145];
 const RIGHT_EYE = [362, 263, 386, 374];
 
 const uniforms = {
-  natsu: { src: 'natsu.png', scale: 6.2, y: 0.33, x: 0.00 },
+  natsu: { src: 'natsu.png', scale: 5.5, y: 0.33, x: 0.00 },
   aihuku: { src: 'aihuku.png', scale: 5.5, y: 0.30, x: -0.01 },
   huyu: { src: 'huyu.png', scale: 5.5, y: 0.29, x: 0.00 },
 };
